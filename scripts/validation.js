e31e8e7014102d252c540651971a5a774d9339f4
Maiah <obards@yahoo.com>
2026-02-14 10:38:23 -0800

    Validation JS

---
+++ scripts/validation.js
@@ -0,0 +1,39 @@
+const showInputError = (formEl, inputEl, errorMsg) => {
+  const errorMsgID = inputEl.id + "-error";
+  const errorMsgEl = document.querySelector("#" + errorMsgID);
+  errorMsgEl.textContent = errorMsg;
+};
+
+const checkInputValidity = (formEl, inputEl) => {
+  console.log(inputEl.validationMessage);
+  if (!inputEl.validity.valid) {
+    showInputError(formEl, inputEl, inputEl.validationMessage);
+  }
+};
+
+const setEventListeners = (formEl) => {
+  const inputList = Array.from(formEl.querySelectorAll(".modal__input"));
+  const buttonElement = formEl.querySelector(".modal__submit-btn");
+
+  console.log(inputList);
+  console.log(buttonElement);
+
+  //   toggleButtonState(inputList, buttonElement);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", function () {
+      checkInputValidity(formEl, inputElement);
+      //   toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll(".modal__form"));
+  console.log(formList);
+  formList.forEach((formEl) => {
+    setEventListeners(formEl);
+  });
+};
+
+enableValidation();
